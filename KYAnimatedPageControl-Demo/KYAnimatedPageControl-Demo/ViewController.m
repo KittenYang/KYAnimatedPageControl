aa4e70097107ba8793cbceaab9d914560599ca24
KittenYang <kittenyang@icloud.com>
2015-06-09 14:48:48 +0800

    initial commit

---
+++ KYAnimatedPageControl-Demo/KYAnimatedPageControl-Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KYAnimatedPageControl-Demo
+//
+//  Created by Kitten Yang on 6/9/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
