8cd43d819af646d9444d2c651f7e8b45f3c92714
ChildhoodAndy <zhaoweibing@didapinche.com>
2015-12-17 14:57:35 +0800

    fix #18
    fix #18

--- KYAnimatedPageControl-Demo/Classes/Line.m
+++ KYAnimatedPageControl-Demo/Classes/Line.m
@@ -183,6 +183,7 @@
     
     self.selectedLineLength = newLineLength;
     anim.delegate = self;
+    anim.removedOnCompletion = YES;
     [self addAnimation:anim forKey:@"lineAnimation"];
 
     self.selectedPage = newIndex;
